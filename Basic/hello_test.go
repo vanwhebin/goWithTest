e275885f44c33e7fd5c199d67faee86ef8b1916e
vanwhebin <vanwhebin@gmail.com>
2022-09-28 17:58:16 +0800

    chapter 01 hello world   TDD 101

--- Basic/hello_test.go
+++ Basic/hello_test.go
@@ -2,11 +2,78 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello world!"
+func TestName(t *testing.T) {
+	got := Hello("David", "")
+	want := "Hello, David"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("David", "")
+		want := "Hello, David"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func TestEmptyHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("David", "")
+		want := "Hello, David"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+}
+
+func gotAssertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestMultiLanguageHello(t *testing.T) {
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Halo, Elodie"
+
+		gotAssertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+
+		gotAssertCorrectMessage(t, got, want)
+	})
 }
--- Basic/helloworld.go
+++ Basic/helloworld.go
@@ -1,11 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Hello() string {
-	return "Hello world!"
+const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Halo, "
+
+func Hello(name string, language string) string {
+
+	if name == "" {
+		name = "world"
+	}
+
+	return getMultiLangHello(language) + name
+}
+
+func getMultiLangHello(language string) (prefix string) {
+
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world", ""))
 }
