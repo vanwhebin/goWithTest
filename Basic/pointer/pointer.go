c0b09ac5b888d5ca77896babec1c5a4e59bf0108
vanwhebin <vanwhebin@gmail.com>
2022-09-30 14:56:34 +0800

    chapter 06 pointer

--- Basic/pointer/pointer.go
+++ Basic/pointer/pointer.go
@@ -1,6 +1,9 @@
 package pointer
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -8,18 +11,25 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-func (w *Wallet) Ballance() Bitcoin {
+var ErrInsufficientFunds = errors.New("cantnot withdraw, insufficient funds")
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
 // 存钱
-func (w *Wallet) Deposit(amount Bitcoin) {
+func (w *Wallet) Deposit(amount Bitcoin) error {
 	w.balance += amount
+	return nil
 }
 
 // 取钱
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
 	w.balance -= amount
+	return nil
 }
 
 //  类型别名
--- Basic/pointer/pointer_test.go
+++ Basic/pointer/pointer_test.go
@@ -1,6 +1,8 @@
 package pointer
 
-import "testing"
+import (
+	"testing"
+)
 
 func assertCorrectBitCoinMessage(t *testing.T, got, want Bitcoin) {
 	t.Helper()
@@ -16,6 +18,25 @@ func assertCorrectBitCoinStringMessage(t *testing.T, got, want Bitcoin) {
 	}
 }
 
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Error("wanted an error but didn't get one")
+		return
+	}
+
+	t.Errorf(err.Error())
+}
+
 func TestWallet(t *testing.T) {
 	t.Run("test deposit", func(t *testing.T) {
 		TestWalletDeposit(t)
@@ -29,7 +50,7 @@ func TestWalletDeposit(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Ballance()
+	got := wallet.Balance()
 	want := Bitcoin(10)
 
 	assertCorrectBitCoinMessage(t, got, want)
@@ -39,8 +60,32 @@ func TestWalletDeposit(t *testing.T) {
 func TestWalletWithdraw(t *testing.T) {
 	wallet := Wallet{balance: Bitcoin(100)}
 	wallet.Withdraw(9)
-	got := wallet.Ballance()
+	got := wallet.Balance()
 	want := Bitcoin(91)
 	assertCorrectBitCoinMessage(t, got, want)
 	assertCorrectBitCoinStringMessage(t, got, want)
+}
+
+func TestTableDrivenWallet(t *testing.T) {
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err)
+	})
+
 }
